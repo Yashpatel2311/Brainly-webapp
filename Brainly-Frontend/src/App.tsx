c25adbba89103f24da1679204e2235d09da4b7b7
Yash Patel <theyashpatel2311@gmail.com>
2025-02-26 23:39:45 +0530

    update code7

--- Brainly-Frontend/src/App.tsx
+++ Brainly-Frontend/src/App.tsx
@@ -1,16 +1,31 @@
 import { Dashboard } from "./pages/dashboard";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Signup />} />
+//         <Route path="/signin" element={<Signin />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
