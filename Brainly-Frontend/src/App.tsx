5ca76bd7c48b8597549388512d17c0759d231071
Yash Patel <theyashpatel2311@gmail.com>
2025-02-27 21:36:21 +0530

    signin page change

--- Brainly-Frontend/src/App.tsx
+++ Brainly-Frontend/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Signin />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
